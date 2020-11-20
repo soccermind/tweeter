dfabfeccc407bda850c26e888cf7a8d2ef396c8c
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-11-19 19:33:09 -0500

    Add responsive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -52,39 +52,10 @@ $(document).ready(function() {
     .then(res => {
       renderTweets(res);
     });
-//     fetchPosts(refreshPosts)
   };
 
   // OLD TWEETS - Fetching tweets with Ajax
   loadTweets();
-  // Fake data taken from initial-tweets.json
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
-
-  // renderTweets(data);
-
 
   // NEW TWEETS - Form submission using jQuery
   $('form').on('submit', event => {
@@ -107,9 +78,8 @@ $(document).ready(function() {
         data: $('form').serialize()
       })
       .then(res => {
-        // console.log(res)
-        // $('#tweets-container').empty();
         $('form textarea').val(null);
+        $('output.counter').val(140);
         loadTweets();
       })
     }
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,12 @@
 $(document).ready(function() {
-  // console.log("form inside the $(document).ready callback!!!")
   $('#tweet-text').on('input', function(event) {
     const count = 140 - this.value.length;
-    // console.log(count);
-    // $('output.counter').text(count);
-    // counter = $(this).parent().find('.counter').text(count);
     const counter = $(this).parent().find('.counter');
     counter.text(count);
-    // console.log(counter);
     if (count < 0) {
       counter.addClass('negative');
-      // counter.removeClass('positive');
     } else {
-      // counter.addClass('positive');
       counter.removeClass('negative');
     }
-    // $(counter).text(count);
   })
 });
